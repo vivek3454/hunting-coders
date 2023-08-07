49befe9b3decb9c64fef598b08ea1ab79085a845
Vivek <vivekparde2411@gmail.com>
2023-08-07 16:47:31 +0530

    created blogs endpoint

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -2,6 +2,11 @@ import { NextResponse } from "next/server";
 import fs from 'fs';
 
 export async function GET(Request) {
-  const file = fs.readdirSync('src/blogdata');
-  return NextResponse.json(file);
+  let allBlogs = [];
+  const filePaths = fs.readdirSync('src/blogdata');
+  filePaths.forEach(filePath => {
+    let file = fs.readFileSync('src/blogdata/' + filePath, 'utf-8');
+    allBlogs.push(JSON.parse(file));
+  });
+  return NextResponse.json(allBlogs);
 }

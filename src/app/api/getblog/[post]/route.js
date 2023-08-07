13581c851675edb0cad552a4a82ede58729b115d
Vivek <vivekparde2411@gmail.com>
2023-08-07 22:40:51 +0530

    changed variable name

--- src/app/api/getblog/[post]/route.js
+++ src/app/api/getblog/[post]/route.js
@@ -4,8 +4,8 @@ import fs from 'fs';
 export async function GET(Request, { params }) {
   try {
     const file = fs.readFileSync(`src/blogdata/${params.post}.json`, 'utf-8');
-    const objectData = JSON.parse(file);
-    return NextResponse.json({ success: true, objectData });
+    const blogData = JSON.parse(file);
+    return NextResponse.json({ success: true, blogData });
   } catch (error) {
     return NextResponse.json({ success: false, message: 'no such blog found'},{ status: 404 });
 }

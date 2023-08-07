817e0dd40c5ad67315364129ad3007febd280710
Vivek <vivekparde2411@gmail.com>
2023-08-07 16:48:19 +0530

    fetched data from created endpoint

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,25 +1,30 @@
+"use client"
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const fetchBlogs = async ()=>{
+      const response = await fetch('http://localhost:3000/api/blogs');
+      const data = await response.json();
+      setBlogs(data);
+    }
+    fetchBlogs();
+  },[])
+  
   return (
     <>
       <section className='max-w-7xl mx-auto px-5 flex justify-center'>
         <div className='mt-10'>
-          <div className="blog mb-5">
-            <Link href={`/blogpost/learn-javascript`}>
-            <h3 className='text-xl mb-1 font-semibold'>How to learn JavaScript in 2023</h3>
+          {blogs.map((blog, i)=>(
+          <div key={i} className="blog mb-5">
+            <Link href={`/blogpost/${blog.slug}`}>
+            <h3 className='text-xl mb-1 font-semibold'>{blog.title}</h3>
             </Link>
-            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit?</p>
+            <p className='text-sm'>{blog.content.substr(0,120)}...</p>
           </div>
-          <div className="blog mb-5">
-            <h3 className='text-xl mb-1 font-semibold'>How to learn JavaScript in 2023</h3>
-            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit?</p>
-          </div>
-          <div className="blog mb-5">
-            <h3 className='text-xl mb-1 font-semibold'>How to learn JavaScript in 2023</h3>
-            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit?</p>
-          </div>
+          ))}
         </div>
       </section>
     </>

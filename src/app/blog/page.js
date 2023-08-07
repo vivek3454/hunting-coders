7fc74449670517102531ef6355f97169e5c0d39b
Vivek <vivekparde2411@gmail.com>
2023-08-07 19:48:10 +0530

    added SSR

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,34 +1,32 @@
-"use client"
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const Blog = () => {
-  const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
-    const fetchBlogs = async ()=>{
-      const response = await fetch('http://localhost:3000/api/blogs');
-      const data = await response.json();
-      setBlogs(data);
-    }
-    fetchBlogs();
-  },[])
-  
+const Blog = async () => {
+  const blogs = await getAllBlogs();
+
   return (
     <>
       <section className='max-w-7xl mx-auto px-5 flex justify-center'>
         <div className='mt-10'>
-          {blogs.map((blog, i)=>(
-          <div key={i} className="blog mb-5">
-            <Link href={`/blogpost/${blog.slug}`}>
-            <h3 className='text-xl mb-1 font-semibold'>{blog.title}</h3>
-            </Link>
-            <p className='text-sm'>{blog.content.substr(0,120)}...</p>
-          </div>
+          {blogs.map((blog, i) => (
+            <div key={i} className="blog mb-5">
+              <Link href={`/blogpost/${blog.slug}`}>
+                <h3 className='text-xl mb-1 font-semibold'>{blog.title}</h3>
+              </Link>
+              <p className='text-sm'>{blog.content.substr(0, 120)}...</p>
+            </div>
           ))}
         </div>
       </section>
     </>
   )
 }
+
+const getAllBlogs = async () => {
+  const response = await fetch('http://localhost:3000/api/blogs', {cache: 'no-store'});
+  const data = await response.json();
+  return data;
+}
+
 
 export default Blog
--- src/app/blogpost/[slug]/page.js
+++ src/app/blogpost/[slug]/page.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const post = ({params}) => {
+const BlogPost = async ({params}) => {
+  const blog = await getSingleBlog(params.slug);
   return (
-    <div>{params.slug}</div>
+    <div className='max-w-7xl mx-auto px-5 mt-10'>
+      <h2 className='text-3xl font-semibold text-center mb-5'>{blog && blog.title}</h2>
+      <div>
+        {blog && blog.content}
+      </div>
+    </div>
   )
 }
 
-export default post
+
+const getSingleBlog = async (slug) => {
+  const response = await fetch(`http://localhost:3000/api/getblog/${slug}`);
+  const {blogData} = await response.json();
+  return blogData;
+}
+
+export default BlogPost

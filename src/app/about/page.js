210131de20eb05d60b77b4d72adc708f0619e72b
Vivek <vivekparde2411@gmail.com>
2023-08-08 16:36:25 +0530

    created about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,21 @@ import React from 'react'
 const About = () => {
   return (
     <>
-      <div>About</div>
+      <div className='max-w-7xl mx-auto px-5'>
+        <h1 className='text-center text-3xl font-semibold mt-10'>About Hunting Coder</h1>
+        <h3 className='text-xl font-semibold mt-5'>Introduction</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit nobis voluptatum quaerat quo. Vero quos, maiores dignissimos, sapiente id veritatis, tempora quibusdam at repellendus molestiae eligendi esse voluptatibus necessitatibus maxime recusandae culpa deleniti ex atque a accusantium. Dolore, ut rem! Illo delectus perspiciatis ad corporis consectetur repellendus, impedit blanditiis corrupti amet assumenda obcaecati rem inventore consequatur eaque vitae temporibus iusto maiores, culpa veritatis. Suscipit velit dolore deserunt ut unde dolores sequi voluptates, molestias facere vel odit ad cum ratione commodi facilis possimus doloribus corrupti! Reiciendis cum praesentium quia vitae vero beatae quis iusto totam quasi suscipit. Et obcaecati dolorum vero!
+        </p>
+        <h3 className='text-xl font-semibold mt-5'>Services offered</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus sed id inventore aliquam molestias quod vitae cumque dolore enim, ut expedita esse odit voluptatibus temporibus nemo libero ipsa vel sequi! Accusamus perferendis similique vitae. Qui voluptates adipisci cum, facere exercitationem quaerat enim debitis eligendi, sit ea rem dicta saepe nihil!
+        </p>
+        <h3 className='text-xl font-semibold mt-5'>Contact Us</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, obcaecati expedita quas placeat ad, nisi culpa, eos modi deserunt minus error animi beatae sequi veritatis quo accusamus minima? Ullam amet repudiandae ex excepturi quos et numquam ipsam iure similique? Facere maxime corporis magni voluptatem eos, enim laudantium at reiciendis possimus asperiores ex, nemo repellat ad tempore, dolor dolore consectetur similique?
+        </p>
+      </div>
     </>
   )
 }

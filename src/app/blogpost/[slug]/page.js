1275ca175370a8f019167299162eac20a3b9f88a
Vivek <vivekparde2411@gmail.com>
2023-08-08 16:43:28 +0530

    added html using dangerouslySetInnerHTML

--- src/app/blogpost/[slug]/page.js
+++ src/app/blogpost/[slug]/page.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import fs from 'fs'
 
-const BlogPost = async ({params}) => {
+const BlogPost = async ({ params }) => {
+  function createMarkUp(c){
+    return {__html: c}
+  }
   const blog = await getSingleBlog(params.slug);
   return (
     <div className='max-w-7xl mx-auto px-5 mt-10'>
       <h2 className='text-3xl font-semibold text-center mb-5'>{blog && blog.title}</h2>
-      <div>
-        {blog && blog.content}
-      </div>
+      {blog && <div dangerouslySetInnerHTML={createMarkUp( blog.content)}></div>}
     </div>
   )
 }
 
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  return [
+    { slug: 'how-to-learn-js' },
+    { slug: 'how-to-learn-nextjs' },
+    { slug: 'how-to-learn-nodejs' },
+    { slug: 'how-to-learn-reactjs' }
+  ]
+}
+
 
 const getSingleBlog = async (slug) => {
-  const response = await fetch(`http://localhost:3000/api/getblog/${slug}`);
-  const {blogData} = await response.json();
+  const file = fs.readFileSync(`src/blogdata/${slug}.json`, 'utf-8');
+  const blogData = JSON.parse(file);
   return blogData;
 }
 
